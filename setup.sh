d25e2982df2119c9bff14ddc24f41db161a9068e
pixnet <pixnet@NC-N2120006736.local>
2019-07-03 11:13:45 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# need sudo su
+apt update
+apt install -y vim
+wget https://raw.githubusercontent.com/a20034294/vim/master/.vimrc > .vimrc
+apt install -y gcc
+apt install -y g++
+apt install -y python3-pip
+apt install -y zsh
+apt install -y powerline fonts-powerline
+chsh -s /bin/zsh
