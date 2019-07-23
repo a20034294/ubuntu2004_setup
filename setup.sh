79f0c841e4ba08f2a5f12c9bf68b5bcc84cc8daa
a20034294 <a20034294@gmail.com>
2019-07-23 14:31:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 # need sudo su
+cd ~/
 sed -i 's/archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y vim
-rm .vimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+rm ~/.vimrc
 wget https://raw.githubusercontent.com/a20034294/vim/master/.vimrc
+vim +PlugInstall +qall > /dev/null
 DEBIAN_FRONTEND=noninteractive apt install -y curl
 DEBIAN_FRONTEND=noninteractive apt install -y git
 DEBIAN_FRONTEND=noninteractive apt install -y tmux
-echo "set -g mouse on" > .tmux.conf
-tmux source-file .tmux.conf
+rm ~/.tmux.conf
+wget https://raw.githubusercontent.com/a20034294/tmux/master/.tmux.conf
+tmux source-file ~/.tmux.conf
 DEBIAN_FRONTEND=noninteractive apt install -y mosh
 DEBIAN_FRONTEND=noninteractive apt install -y gcc
 DEBIAN_FRONTEND=noninteractive apt install -y g++
 DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
-pip3 install numpy
-pip3 install matplotlib
 DEBIAN_FRONTEND=noninteractive apt install -y htop
 DEBIAN_FRONTEND=noninteractive apt install -y zsh
 DEBIAN_FRONTEND=noninteractive apt install -y powerline
