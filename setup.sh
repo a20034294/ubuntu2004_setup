2d196bed97a4b6dc9bb75365fa33ac8ad5afd363
a20034294 <a20034294@gmail.com>
2019-07-24 21:46:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,17 @@
 # need sudo su
-cd ~/
 sed -i 's/archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-rm ~/.vimrc
+git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
+rm .vimrc
 wget https://raw.githubusercontent.com/a20034294/vim/master/.vimrc
 vim +PlugInstall +qall > /dev/null
 DEBIAN_FRONTEND=noninteractive apt install -y curl
 DEBIAN_FRONTEND=noninteractive apt install -y git
 DEBIAN_FRONTEND=noninteractive apt install -y tmux
-rm ~/.tmux.conf
+rm .tmux.conf
 wget https://raw.githubusercontent.com/a20034294/tmux/master/.tmux.conf
-tmux source-file ~/.tmux.conf
+tmux source-file .tmux.conf
 DEBIAN_FRONTEND=noninteractive apt install -y mosh
 DEBIAN_FRONTEND=noninteractive apt install -y gcc
 DEBIAN_FRONTEND=noninteractive apt install -y g++
