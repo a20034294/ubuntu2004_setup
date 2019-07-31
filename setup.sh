2bee94410e6d65c21a8cdfb08d5564ec5599c2ac
a20034294 <a20034294@gmail.com>
2019-07-31 17:07:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,11 +21,12 @@ DEBIAN_FRONTEND=noninteractive apt install -y zsh
 DEBIAN_FRONTEND=noninteractive apt install -y powerline
 DEBIAN_FRONTEND=noninteractive apt install -y fonts-powerline
 DEBIAN_FRONTEND=noninteractive apt install -y locales
-locale-gen zh_TW.UTF-8
+locale-gen en_US.UTF-8
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 echo "alias ls='ls -alF --color=always'" >> .zshrc
 sed -i 's/robbyrussell/agnoster/g' .zshrc
+sed -i 's/git/git mosh/g' .zshrc
 chsh -s /bin/zsh
-export LC_ALL="zh_TW.UTF-8"
+export LC_ALL="en_US.UTF-8"
 zsh
 
