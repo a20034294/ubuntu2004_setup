61abe713da48a8d6bc361eaaf9195ee05a760264
a20034294 <a20034294@gmail.com>
2019-07-31 16:40:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive apt install -y vim
 git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
 rm .vimrc
 wget https://raw.githubusercontent.com/a20034294/vim/master/.vimrc
-vim +PlugInstall +qall > /dev/null
+vim +PluginInstall +qall
 DEBIAN_FRONTEND=noninteractive apt install -y tmux
 rm .tmux.conf
 wget https://raw.githubusercontent.com/a20034294/tmux/master/.tmux.conf
@@ -20,5 +20,11 @@ DEBIAN_FRONTEND=noninteractive apt install -y htop
 DEBIAN_FRONTEND=noninteractive apt install -y zsh
 DEBIAN_FRONTEND=noninteractive apt install -y powerline
 DEBIAN_FRONTEND=noninteractive apt install -y fonts-powerline
+DEBIAN_FRONTEND=noninteractive apt install -y locales
+locale-gen en_US.UTF-8
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 echo "alias ls='ls -alF --graph=auto'" >> .zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i 's/robbyrussell/agnoster/g' .zshrc
+chsh -s /bin/zsh
+zsh
+
