9405a1764552a408ee05dd72fd4bae8395475432
a20034294 <a20034294@gmail.com>
2020-03-06 15:42:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
 # need sudo
 sed -i 's/archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y curl
-DEBIAN_FRONTEND=noninteractive apt install -y git
-DEBIAN_FRONTEND=noninteractive apt install -y vim
+sudo DEBIAN_FRONTEND=noninteractive apt install -y curl
+sudo DEBIAN_FRONTEND=noninteractive apt install -y git
+sudo DEBIAN_FRONTEND=noninteractive apt install -y vim
 git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
 rm .vimrc
 wget https://raw.githubusercontent.com/a20034294/vim/master/.vimrc
 vim +PluginInstall +qall
 git config --global core.editor "vim"
-DEBIAN_FRONTEND=noninteractive apt install -y tmux
+sudo DEBIAN_FRONTEND=noninteractive apt install -y tmux
 rm .tmux.conf
 wget https://raw.githubusercontent.com/a20034294/tmux/master/.tmux.conf
 tmux source-file .tmux.conf
-DEBIAN_FRONTEND=noninteractive apt install -y mosh
-DEBIAN_FRONTEND=noninteractive apt install -y gcc
-DEBIAN_FRONTEND=noninteractive apt install -y g++
-DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
-DEBIAN_FRONTEND=noninteractive apt install -y htop
-DEBIAN_FRONTEND=noninteractive apt install -y net-tools
-DEBIAN_FRONTEND=noninteractive apt install -y zsh
-DEBIAN_FRONTEND=noninteractive apt install -y powerline
-DEBIAN_FRONTEND=noninteractive apt install -y fonts-powerline
-DEBIAN_FRONTEND=noninteractive apt install -y locales
+sudo DEBIAN_FRONTEND=noninteractive apt install -y mosh
+sudo DEBIAN_FRONTEND=noninteractive apt install -y gcc
+sudo DEBIAN_FRONTEND=noninteractive apt install -y g++
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
+sudo DEBIAN_FRONTEND=noninteractive apt install -y htop
+sudo DEBIAN_FRONTEND=noninteractive apt install -y net-tools
+sudo DEBIAN_FRONTEND=noninteractive apt install -y zsh
+sudo DEBIAN_FRONTEND=noninteractive apt install -y powerline
+sudo DEBIAN_FRONTEND=noninteractive apt install -y fonts-powerline
+sudo DEBIAN_FRONTEND=noninteractive apt install -y locales
 locale-gen en_US.UTF-8
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 echo "alias ls='ls -alF --color=always'" >> .zshrc
